62a61e2b93c47c902903196be4ca8bdedba47194
Jacek Kitlinski <gasniczka@o2.pl>
2022-08-13 15:09:24 +0200

    articleRepository

---
+++ src/main/java/org/example/blog/ports/ArticleRepository.java
@@ -0,0 +1,23 @@
+package org.example.blog.ports;
+
+import org.example.blog.domain.Article;
+import org.example.blog.domain.ArticleMode;
+import org.example.blog.domain.Tag;
+
+import java.util.Collection;
+
+
+public interface ArticleRepository {
+
+    void save(Article article);
+
+    Collection<Article> getByCategory(String category);
+
+    Collection<Article> getByTags(Tag tag);
+
+    Article getByUrl(String url);
+
+    void switchMode(String id, ArticleMode mode);
+
+
+}
